12bf51e15d77fd1f1f3678e8b185294fc39bd160
kingsmen1 <90970163+kingsmen1@users.noreply.github.com>
2022-12-10 19:48:23 +0530

    index updated

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,10 @@
 /* eslint-disable */
-console.log('hellow from parcel');
+console.log('index.js');
 
-// import 'core-js/stable';
-// // import axios from 'axios';
-// import { login, logout } from './login';
-// import { displayMap } from './mapbox';
-// import { updateData, updateSettings } from './updateSetting';
-// import { bookTour } from './stripe';
+const hideAlert = () => {
+  const el = document.querySelector('.alert');
+  if (el) el.parentElement.removeChild(el);
+};
 const showAlert = (type, msg) => {
   hideAlert();
   const markup = `<div class="alert alert--${type}">${msg}</div>`;
@@ -28,6 +26,63 @@ if (mapBox) {
     document.getElementById('map').dataset.locations
   );
   console.log(locations);
+  const displayMap = (locations) => {
+    mapboxgl.accessToken =
+      'pk.eyJ1Ijoib3NtYW43ODYiLCJhIjoiY2xhOGY5d3VyMDJoczNvbnZpdGI0eDJiNSJ9.EYSlAQKesirYTHeEIi2wtg';
+    var map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/light-v10',
+      scrollZoom: false,
+      //   center: [-118.113491, 34.111745],
+      //   zoom: 10,
+      //by interactive map cannot be moved.
+      // interactive: false,
+    });
+
+    const bounds = new mapboxgl.LngLatBounds();
+
+    locations.forEach((loc) => {
+      //Create Marker
+      const el = document.createElement('div');
+      el.className = 'marker';
+
+      //Add Marker
+      new mapboxgl.Marker({
+        element: el,
+        anchor: 'bottom',
+      })
+        .setLngLat(loc.coordinates)
+        .addTo(map);
+
+      new mapboxgl.Popup({
+        offset: 30,
+      })
+        .setLngLat(loc.coordinates)
+        .setHTML(
+          `<p>
+          Day ${loc.day}: ${loc.description}
+        </p>`
+        )
+        .addTo(map);
+
+      //Extend map bounds to include current location
+      bounds.extend(loc.coordinates);
+    });
+
+    //To fit map to all bounds/coordinates.
+    map.fitBounds(bounds, {
+      padding: {
+        top: 200,
+        bottom: 200,
+        left: 200,
+        right: 200,
+      },
+    });
+
+    //To add Zoom Buttons
+    const nav = new mapboxgl.NavigationControl();
+    map.addControl(nav, 'top-right');
+  };
   displayMap(locations);
 }
 
@@ -36,9 +91,8 @@ if (loginForm) {
     e.preventDefault();
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
-    console.log(email, password);
+
     const login = async (email, password) => {
-      console.log('login');
       try {
         //axios is used to send http methods from client side to server.
         const res = await axios({
@@ -49,8 +103,6 @@ if (loginForm) {
             password,
           },
         });
-        console.log('after axios');
-        console.log(res.data.status);
 
         if (res.data.status === 'success') {
           showAlert('success', 'Logged in successfully!');
@@ -67,7 +119,7 @@ if (loginForm) {
   });
 }
 
-if (logOutButton)
+if (logOutButton) {
   logOutButton.addEventListener('click', async () => {
     console.log('logout function played');
     try {
@@ -82,6 +134,7 @@ if (logOutButton)
       showAlert('error', 'Error logging out! Try Again .');
     }
   });
+}
 
 if (userDataForm) {
   userDataForm.addEventListener('submit', (e) => {
@@ -91,7 +144,7 @@ if (userDataForm) {
     form.append('email', document.getElementById('email').value);
     //Since we receave files in an array we have to select by index.
     form.append('photo', document.getElementById('photo').files[0]);
-    async (data, type) => {
+    const updateSettings = async (data, type) => {
       console.log(data, type);
       try {
         const url =
@@ -115,7 +168,7 @@ if (userDataForm) {
         showAlert('error', err.response.data.message);
       }
     };
-    // updateSettings(form, 'data');
+    updateSettings(form, 'data');
   });
 }
 
@@ -126,6 +179,30 @@ if (userPasswordForm) {
     const passwordCurrent = document.getElementById('password-current').value;
     const password = document.getElementById('password').value;
     const confirmPassword = document.getElementById('password-confirm').value;
+    const updateSettings = async (data, type) => {
+      console.log(data, type);
+      try {
+        const url =
+          type === 'data'
+            ? '/api/v1/users/updateMe'
+            : '/api/v1/users/updateMyPassword';
+        const res = await axios({
+          method: 'PATCH',
+          url,
+          data: data,
+        });
+        console.log(res.status);
+        if (res.status === 200) {
+          showAlert('success', `${type} Data Updated successfully`);
+          //page reloading.
+          window.setTimeout(() => {
+            location.reload();
+          }, 1000);
+        }
+      } catch (err) {
+        showAlert('error', err.response.data.message);
+      }
+    };
     await updateSettings(
       { passwordCurrent, password, confirmPassword },
       'password'
@@ -143,6 +220,26 @@ if (tourBookButton) {
     e.target.textContent = 'Processing...';
     //^ "tour-id" given from template converts to tourId in js bcos after '-' it cammel case it.
     const { tourId } = e.target.dataset;
+    const stripe = Stripe(
+      'pk_test_51M9nnrSIH2aTZA92VigBxG04qGjmY9mETYcYcmF96MxXNo8PJyqiC3VK6zxS9DQuLuv50bvCX47fedrfRPPfSOEB00UlBeWiuT'
+    );
+    const bookTour = async (tourId) => {
+      try {
+        //1) Get Checkout sessoin from api.
+        //^ Without parameter's axios is set default to 'get'
+        const session = await axios(
+          `/api/v1/booking/checkout-session/${tourId}`
+        );
+
+        //2) Create Checkout form + Charge Credit Card.
+        await stripe.redirectToCheckout({
+          sessionId: session.data.session.id,
+        });
+      } catch (e) {
+        console.log(err);
+        showAlert('error', err);
+      }
+    };
     bookTour(tourId);
   });
 }
